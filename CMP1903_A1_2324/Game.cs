51f63d31e1e1797eb197ecc47653c835fcb09895
Joshdunny <joshdunny2005@gmail.com>
2024-03-01 00:45:38 +0000

    Created the basics to the game class
    Has the required constructor which creates 3 die objects rolls all 3 summing them until the user does not want to

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -16,6 +16,37 @@ namespace CMP1903_A1_2324
          */
 
         //Methods
+        public Game()
+        {
+            Die die1 = new Die();
+            Die die2 = new Die();
+            Die die3 = new Die();
+            int sum = -1;
 
+
+            bool roll = true;
+
+            while(roll) 
+            {
+                die1.RollDie();
+                die2.RollDie();
+                die3.RollDie();
+
+                sum = die1.DieValue + die2.DieValue + die1.DieValue;
+                Console.WriteLine("The sum of the rolled dice >>> {0}", sum);
+                
+                string Rollquery = "";
+                while (Rollquery != "Y" || Rollquery != "N")
+                {
+                    Console.WriteLine("Would you like to roll again, [Y] or [N]");
+                    Rollquery = Console.ReadLine();
+                }
+
+                if (Console.ReadLine() == "N")
+                {
+                    roll = false;
+                }
+            }
+        }
     }
 }
