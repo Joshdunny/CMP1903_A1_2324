11c2123f81455e11beafe99e440a8650b8efcb19
Joshdunny <joshdunny2005@gmail.com>
2024-03-01 19:59:35 +0000

    Fixed Encapsulation issue & Random
    Encapsulation was improperly implemented causing a stack overflow error it is now fixed and DieValue generated would be the same for all 3 due to the same seed being used Thread.sleep is added to fix that issue

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
@@ -15,19 +16,26 @@ namespace CMP1903_A1_2324
 
         //Property(s)
         //DieValue property holding the value of the die
+        public int dieValue;
         public int DieValue
         {
             //encapsulated using get an set methods 
-            get{ return DieValue; }
-            set{ DieValue = value; }
+            get{ return dieValue; }
+            set{ dieValue = value; }
         }
         //Method(s)            
         //RollDie methods "rolls" the die generating a random value between 1,6
         public int RollDie()
         {
+            
             //generates random int between 1,6
             Random r = new Random();
             DieValue = r.Next(1, 7);
+
+            //Used to get the program to wait between generating a random number
+            //Ensuring the same seed is not used for all die rolled
+            Thread.Sleep(100);
+
             //returns integer DieValue
             return DieValue;
         }
