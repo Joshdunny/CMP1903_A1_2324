389a2c9b32c1fd2020c05be7c8b29e2e2a161c4e
Joshdunny <joshdunny2005@gmail.com>
2024-03-03 00:52:17 +0000

    Finished porgram to call all methods and display finish to program

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -17,7 +17,18 @@ namespace CMP1903_A1_2324
 
             //creates the game object calling the Game constructor 
             Game startGame = new Game();
+            //calls the gamestart method to begin the game and rolling the dice
             startGame.GameStart();
+
+            //creates the testing object for verifying the testing the Game and Die classes
+            Testing test = new Testing();
+            //calls both methods to test game class and die class 
+            test.TestingGameClass();
+            test.TestingDieClass();
+
+            //Displays the end of the program
+            Console.WriteLine("" +
+                " <<< Program has finished >>>");
             Console.ReadLine();
         }
     }
